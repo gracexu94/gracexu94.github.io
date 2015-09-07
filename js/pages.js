9c0399ad334a785bb19916bbed1a3b2ec97ae4a7
gracexu94 <serihppas@gmail.com>
2015-09-07 19:26:17 -0400

    Begin website redo

---
+++ js/pages.js
@@ -0,0 +1,9 @@
+function showAbout() {
+  alert("howdy");
+  $("#why").html("j,,,,,");
+  doMove();
+};
+
+function doMove() {
+  foo.style.left = (foo.style.left+10)+'px'; // pseudo-property code: Move right by 10px
+}
