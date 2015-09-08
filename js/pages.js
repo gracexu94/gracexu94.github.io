3d481518037808b0ec482c7e5c5b5addf7e67b55
gracexu94 <serihppas@gmail.com>
2015-09-07 21:59:38 -0400

    Add proto about

--- js/pages.js
+++ js/pages.js
@@ -1,9 +1,79 @@
-function showAbout() {
-  alert("howdy");
-  $("#why").html("j,,,,,");
-  doMove();
-};
+var moveElements = function() {
+  $( ".GX" ).animate({
+    marginRight: "90%",
+    marginTop: "-2.2in",
+  }, 800 );
 
-function doMove() {
-  foo.style.left = (foo.style.left+10)+'px'; // pseudo-property code: Move right by 10px
+  $("#GXpic").animate({
+    height: "120px",
+    margin: "10px 0px 0px 20px",
+  }, 800 );
+
+  $(".bottom").animate({
+    left: "50px",
+    top: "-35px",
+    minWidth: "150px",
+    width: "150px",
+  }, 800);
+
+  moveSidebar();
 }
+
+var moveSidebar = function() {
+  var styles = {
+    position: "relative",
+    left: "10px",
+    top: "-30px",
+    color: "white",
+    padding: "5px 20px 5px 20px",
+    margin: "5px",
+  }
+
+  var styles2 = {
+    display: "block",
+    width: "200px",
+    padding: "10px",
+    margin: "5px 0px 5px 0px",
+  }
+
+  $(".s").css(styles2);
+  $(".sidebar").css(styles);
+  $(".bottomheader").css("margin", "25px 0px 10px 0px");
+}
+
+var homePg = true;
+$("#about").click(function() {
+  alert("hy");
+  moveElements();
+  homePg = false;
+  $("#contactpg").hide();
+  $("#aboutmepg").show(900);
+ });
+
+$("#GXpic").click(function() {
+  if (!homePg) {
+    location.reload();
+  }
+});
+
+$("#resume").click(function() {
+  window.open('resume.pdf');
+});
+
+$("#contact").click(function() {
+    if (homePg) {
+    moveElements();
+    homePg = false;
+    $("#contactpg").show(900);
+  } else {
+    $("#aboutmepg").hide();
+    $("#contactpg").show(900);
+  }
+});
+
+$("#work").click(function() {
+    moveElements();
+    homePg = false;
+    //$(".container").css("visibility","");
+    $("#gallery").css("visibility","");
+});
